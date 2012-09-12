a258d1b5591f2b9bbca5b7a5b91b2e319ecf35fe
LomnickiLucas <LucasLomnicki@gmail.com>
2012-09-12 11:25:06 -0400

    Implemented beggings of new layout

---
+++ Models/StudentResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdvisementSys.Models
+{
+    public class StudentResponseModel
+    {
+        public IEnumerable<student> _student { get; set; }
+
+        public IEnumerable<issue> _issue { get; set; }
+
+        public IEnumerable<program> _program { get; set; }
+
+        public IEnumerable<note> _programm { get; set; }
+    }
+}
