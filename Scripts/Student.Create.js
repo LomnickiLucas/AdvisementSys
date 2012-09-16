73a115537301a42bbc22abd998144452aa0202ee
Justin Abeto <justin.abeto@gmail.com>
2012-09-16 16:15:48 -0400

    BlockUI

---
+++ Scripts/Student.Create.js
@@ -0,0 +1,10 @@
+﻿$(function () {
+    $("form").submit(function (e) {
+        $.post($(this).attr("action"),
+    $(this).serialize(),
+    function (data) {
+        openBlock(data);
+    });
+        e.preventDefault();
+    });
+});
