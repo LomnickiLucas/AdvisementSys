44bec53550c9a26e92ec11eb71510f5b3097d36b
LomnickiLucas <LucasLomnicki@gmail.com>
2012-09-17 16:17:48 -0400

    Further Feature have been implemented
    Default full time status and Enrolled have been checked in the create
    studen tview.
    Added Student ID into the Student Index Results.
    Add Province Drop Down List to Edit and create Studetn.
    Fields now persist between searches.

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AdvisementSys.Models;
+using AdvisementSys.Models.Request;
 using System.Text;
 
 namespace AdvisementSys.Controllers
@@ -28,7 +29,8 @@ namespace AdvisementSys.Controllers
             }
             ViewBag.programcode = new SelectList(collection);
             var students = db.students.Include("campu").Include("program");
-            return View(students.ToList());
+            IndexStudentRequestModel model = new IndexStudentRequestModel() { _student = students.ToList(), programCode = "Please Select a Program", campus = "Please Select a Campus", enrolled = true };
+            return View(model);
         }
 
         //
@@ -45,6 +47,7 @@ namespace AdvisementSys.Controllers
                 collections.Add(pcode.programcode.ToString().Trim() + " - " + pcode.programname.ToString().Trim());
             }
             ViewBag.programcode = new SelectList(collections);
+            IndexStudentRequestModel model = new IndexStudentRequestModel() { studentID = collection.Get(0).Trim(), fName = collection.Get(1).Trim(), lName = collection.Get(2).Trim(), programCode = "Please Select a Program", campus = "Please Select a Campus", email = collection.Get(5).Trim() };
             var students = db.students.Include("campu").Include("program");
             IEnumerable<student> studentz = students;
             if (!collection.Get(0).Equals(""))
@@ -57,19 +60,33 @@ namespace AdvisementSys.Controllers
             {
                 StringBuilder sb = new StringBuilder(collection.Get(3).Trim().ToUpper());
                 sb.Remove(5, sb.Length - 5);
+                model.programCode = collection.Get(3).Trim();
                 studentz = studentz.Where(stud => stud.programcode.Trim().ToUpper().Contains(sb.ToString()));
             }
             if (!collection.Get(4).Equals(""))
+            {
+                model.campus = collection.Get(4).Trim();
                 studentz = studentz.Where(stud => stud.campus.Trim().ToUpper().Contains(collection.Get(4).Trim().ToUpper()));
+            }
             if (!collection.Get(5).Equals(""))
                 studentz = studentz.Where(stud => stud.email.Trim().ToUpper().Contains(collection.Get(5).Trim().ToUpper()));
             if (collection.Get(6).Equals("true,false"))
+            {
+                model.acadprobation = true;
                 studentz = studentz.Where(stud => stud.acadprobation.Equals(true));
+            }
             if (collection.Get(7).Equals("true,false"))
+            {
+                model.fulltimestatus = true;
                 studentz = studentz.Where(stud => stud.fulltimestatus.Equals(true));
+            }
             if (collection.Get(8).Equals("true,false"))
+            {
+                model.enrolled = true;
                 studentz = studentz.Where(stud => stud.enrolled.Equals(true));
-            return View(studentz);
+            }
+            model._student = studentz;
+            return View(model);
         }
 
         //
@@ -104,7 +121,16 @@ namespace AdvisementSys.Controllers
         {
             ViewBag.campus = new SelectList(db.campus, "cname", "cname");
             ViewBag.programcode = new SelectList(db.programs, "programcode", "programname");
-            return View();
+
+            //var program = db.programs;
+            //List<String> collection = new List<String>();
+            //foreach (program pcode in program)
+            //{
+            //    collection.Add(pcode.programcode.ToString().Trim() + " - " + pcode.programname.ToString().Trim());
+            //}
+            //ViewBag.programcode = new SelectList(collection);
+            student student = new student() { enrolled = true, fulltimestatus = true };
+            return View(student);
         } 
 
         //
@@ -144,6 +170,13 @@ namespace AdvisementSys.Controllers
                     student student = db.students.Single(s => s.studentid == id);
                     ViewBag.campus = new SelectList(db.campus, "cname", "cname", student.campus);
                     ViewBag.programcode = new SelectList(db.programs, "programcode", "programname", student.programcode);
+                    //var program = db.programs;
+                    //List<String> collection = new List<String>();
+                    //foreach (program pcode in program)
+                    //{
+                    //    collection.Add(pcode.programcode.ToString().Trim() + " - " + pcode.programname.ToString().Trim());
+                    //}
+                    //ViewBag.programcode = new SelectList(collection);
                     return View(student);
                 }
                 else
---
+++ Models/Request/IndexStudentRequestModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdvisementSys.Models.Request
+{
+    public class IndexStudentRequestModel
+    {
+        public IEnumerable<student> _student { get; set; }
+
+        public String studentID { get; set; }
+
+        public String fName { get; set; }
+
+        public String lName { get; set; }
+
+        public String programCode { get; set; }
+
+        public String campus { get; set; }
+
+        public String email { get; set; }
+
+        public Boolean acadprobation { get; set; }
+
+        public Boolean fulltimestatus { get; set; }
+
+        public Boolean enrolled { get; set; }
+    }
+}
