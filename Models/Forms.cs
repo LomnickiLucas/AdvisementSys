eef4779710a4566c4b40aa0a43e47749a4159c93
LomnickiLucas <LucasLomnicki@gmail.com>
2012-09-24 19:23:24 -0400

    Forms
    The Readmission Form is not complete

---
+++ Models/Forms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdvisementSys.Models
+{
+    public class Forms
+    {
+        public DateTime Date { get; set; }
+
+        public String FormType { get; set; }
+
+        public String URL { get; set; }
+    }
+}
