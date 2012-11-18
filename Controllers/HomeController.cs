04baaab243cffbdd1e3a38c90359174a825dde1b
LomnickiLucas <LucasLomnicki@gmail.com>
2012-11-17 23:44:17 -0500

    added a basic homepage
    I am aware the date is not workingwith  in the kendo grid will fix later

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,17 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AdvisementSys.Models;
 
 namespace AdvisementSys.Controllers
 {
     public class HomeController : Controller
     {
+        private Entities db = new Entities();
+
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
-            return View();
+            DateTime date = DateTime.Today.AddDays(7);
+            DateTime date2 = DateTime.Now.AddMonths(-1);
+            IEnumerable<appointment> appointments = db.appointments.Where(i => i.employeeid == User.Identity.Name && i.starttime >= DateTime.Today && i.starttime <= date);
+            IEnumerable<issue> issues = db.issues.Where(i => i.date <= date2 && i.status != "Complete");
+            List<Events> Events = new List<Events>();
+            foreach (appointment appointment in appointments)
+            {
+                Events Event = new Events();
+                Event.id = appointment.appointmentid;
+                Event.title = appointment.subject;
+                Event.allDay = false;
+                Event.start = ConvertToUnixTimestamp(appointment.starttime).ToString();
+                Event.end = ConvertToUnixTimestamp(appointment.endtime).ToString();
+                Event.url = Url.Action("Details/" + appointment.appointmentid.ToString(), "Calendar");
+                
+                Events.Add(Event);
+            }
+
+            List<IssuesPOCO> _issues = new List<IssuesPOCO>();
+            foreach (issue i in issues.OrderByDescending(e => e.date))
+            {
+                IssuesPOCO temp = new IssuesPOCO();
+                temp.IssueID = i.issueid;
+                temp.Name = i.issuename;
+                temp.Date = "\"date\":\"" + i.date.Year + "-" + i.date.Month + "-" + i.date.Day + "\"";
+                temp.Status = i.status;
+                temp.Urgency = i.urgency;
+
+                _issues.Add(temp);
+            }
+
+            IndexHomeModel model = new IndexHomeModel() { _Events = Events, _Issues = _issues };
+
+            return View(model);
+        }
+
+        private double ConvertToUnixTimestamp(DateTime value)
+        {
+            //create Timespan by subtracting the value provided from
+            //the Unix Epoch
+            TimeSpan span = (value - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
+
+            //return the total seconds (which is a UNIX timestamp)
+            return (double)span.TotalSeconds;
         }
 
     }
---
+++ Models/Request/IndexHomeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdvisementSys.Models;
+
+namespace AdvisementSys.Controllers
+{
+    public class IndexHomeModel
+    {
+        public IEnumerable<Events> _Events { get; set; }
+
+        public IEnumerable<IssuesPOCO> _Issues { get; set; }
+    }
+}
