382532f0abf755e95f03b3f793037a2c89ea8174
LomnickiLucas <LucasLomnicki@gmail.com>
2012-12-03 21:39:39 -0500

    Roles to the server

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -19,6 +19,8 @@ namespace AdvisementSys.Controllers
 
         public ActionResult LogOn()
         {
+            Roles.CreateRole("Receptionist");
+            Roles.CreateRole("Student Advisor");
             return View();
         }
 
